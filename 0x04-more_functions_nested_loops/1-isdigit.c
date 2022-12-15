5db73ea7071fdc38418061eb70d370481dc37739
Dctenda <dcpatsa@gmail.com>
2022-12-15 13:34:32 -0800

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,25 +1,15 @@
 #include "main.h"
 
-#include <unistd.h>
-
 /**
  *
- *  _putchar - writes the character c to stdout
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
  *
- * @c: The character to print
- *
- * Return: On success 1.
- *
- * On error, -1 is returned, and errno is set appropriately.
- *
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
-int _putchar(char c)
-
+int _isdigit(int c)
 {
 
-	 return (write(1, &c, 1));
+	 return (c >= '0' && c <= '9');
 
 }
-
-
