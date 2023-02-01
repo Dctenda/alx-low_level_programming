aced4ef5c4749d7e646c7a5ef16314130da1cec4
Dctenda <dcpatsa@gmail.com>
2023-01-31 18:32:37 -0800

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,11 +5,13 @@
  *
  *   @head: pointer to the first element in the linked list
  *
- * Return: the data inside the elements that was deleted,
+ *    *
  *
- * or 0 if the list is empty
+ *     * Return: the data inside the elements that was deleted,
  *
- */
+ *      * or 0 if the list is empty
+ *
+ *       */
 
 int pop_listint(listint_t **head)
 
@@ -39,4 +41,4 @@ int pop_listint(listint_t **head)
 
 	        return (num);
 
-
+}
